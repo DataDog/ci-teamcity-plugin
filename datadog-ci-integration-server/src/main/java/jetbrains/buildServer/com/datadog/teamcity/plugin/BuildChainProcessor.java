453e35b8be5aacd68649e69763cd50c712ebe7fc
andrea-mosk <39099407+andrea-mosk@users.noreply.github.com>
2022-12-21 15:36:20 +0100

    Revert "[CIAPP-5536] Adding copyright headers to code files"

--- datadog-ci-integration-server/src/main/java/jetbrains/buildServer/com/datadog/teamcity/plugin/BuildChainProcessor.java
+++ datadog-ci-integration-server/src/main/java/jetbrains/buildServer/com/datadog/teamcity/plugin/BuildChainProcessor.java
@@ -1,10 +1,3 @@
-/**
- * Unless explicitly stated otherwise all files in this repository are licensed
- * under the Apache License Version 2.0.
- * This product includes software developed at Datadog (https://www.datadoghq.com/)
- * Copyright 2022-present Datadog, Inc.
- */
-
 package jetbrains.buildServer.com.datadog.teamcity.plugin;
 
 import jetbrains.buildServer.com.datadog.teamcity.plugin.ProjectHandler.ProjectParameters;
